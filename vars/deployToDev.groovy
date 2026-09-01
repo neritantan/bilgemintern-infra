8f7ef9fb4686517f685730ad8710cc31a9df2dce
neritantan <enesibrahim58@gmail.com>
2026-09-01 16:24:54 +0300

    refactor(ci): redeploy dev stack with compose

--- vars/deployToDev.groovy
+++ vars/deployToDev.groovy
@@ -1,19 +1,12 @@
-def call(Map cfg) {
-    withCredentials([
-        sshUserPrivateKey(credentialsId: 'server-ssh', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER'),
-        usernamePassword(credentialsId: 'ghcr-login', passwordVariable: 'GHCR_PAT', usernameVariable: 'GHCR_USER')
-    ]) {
-
-        def img = "ghcr.io/${env.GHCR_USER}/${cfg.image}:${cfg.tag}"
-
-        sh """
-        ssh -i \$SSH_KEY -o StrictHostKeyChecking=no \$SSH_USER@dev '
-            docker stop ${cfg.name} || true
-            docker rm ${cfg.name} || true
-            docker pull ${img}
-            docker run -d --name ${cfg.name} -p ${cfg.ports} ${img}
+def call() {
+    withCredentials([sshUserPrivateKey(credentialsId: 'server-ssh', keyFileVariable: 'SSH_KEY', usernameVariable: 'SSH_USER')]) {
+        sh '''
+        ssh -i $SSH_KEY -o StrictHostKeyChecking=no $SSH_USER@dev '
+            docker compose pull
+            docker compose down
+            docker compose up -d
             docker image prune -a -f --filter "until=24h"
         '
-        """
+        '''
     }
 }
