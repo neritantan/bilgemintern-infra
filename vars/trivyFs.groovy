a3db50b4f48070c34d94e46dc7c4b0f3ce22cc56
neritantan <enesibrahim58@gmail.com>
2026-09-02 14:06:40 +0300

    refactor(ci): gate trivy on critical findings only

--- vars/trivyFs.groovy
+++ vars/trivyFs.groovy
@@ -1,3 +1,4 @@
 def call() {
-    sh 'docker run --rm -v $WORKSPACE:/scan -v /var/jenkins_home/trivy-cache:/root/.cache/trivy aquasec/trivy:0.74.0 fs --severity HIGH,CRITICAL --ignore-unfixed --exit-code 1 /scan'
+    sh 'docker run --rm -v $WORKSPACE:/scan -v /var/jenkins_home/trivy-cache:/root/.cache/trivy aquasec/trivy:0.74.0 fs --severity HIGH,CRITICAL --ignore-unfixed /scan'
+    sh 'docker run --rm -v $WORKSPACE:/scan -v /var/jenkins_home/trivy-cache:/root/.cache/trivy aquasec/trivy:0.74.0 fs --severity CRITICAL --ignore-unfixed --exit-code 1 --quiet /scan'
 }
--- vars/trivyImage.groovy
+++ vars/trivyImage.groovy
@@ -1,3 +1,4 @@
 def call(String image) {
-    sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /var/jenkins_home/trivy-cache:/root/.cache/trivy aquasec/trivy:0.74.0 image --severity HIGH,CRITICAL --ignore-unfixed --exit-code 1 ${image}"
+    sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /var/jenkins_home/trivy-cache:/root/.cache/trivy aquasec/trivy:0.74.0 image --severity HIGH,CRITICAL --ignore-unfixed ${image}"
+    sh "docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /var/jenkins_home/trivy-cache:/root/.cache/trivy aquasec/trivy:0.74.0 image --severity CRITICAL --ignore-unfixed --exit-code 1 --quiet ${image}"
 }
